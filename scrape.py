79621f3c038beb37a4e442c0e424df0ec3d0cd4b
mc <goat@goat.rocks>
2021-03-03 23:01:54 -0500

    add cvs (thnx to david!) and fix walgreens

--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import os
 from twilio.rest import Client
 import json
+import datetime
 
 load_dotenv()
 
@@ -71,30 +72,73 @@ def get_pages(vaxsite, url):
         #notify(vaxsite, "This site has " + str(total_appts) + " appointments! " + url )
         sms("This site has " + str(total_appts) + " appointments! " + url )
             
-def walgreens():
-    post_content = '{"serviceId":"99","position":{"latitude":42.4326041,"longitude":-71.0557196},"appointmentAvailability":{"startDateTime":"2021-02-26"},"radius":25}'
-    post_dict = json.loads(post_content)
-    post_response = requests.post("https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability", json=post_dict)
-    print(post_response)
+def walgreens(client):
+    today = datetime.date.today()
+    post_content = {
+        "serviceId": "99",
+        "position": {
+            "latitude": 42.4326041,
+            "longitude": -71.0557196
+        },
+        "appointmentAvailability": {
+            "startDateTime": today.strftime('%Y-%m-%d')
+        },
+        "radius": 25
+    }
+    post_response = client.post("https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability", json=post_content, headers=dict(Referer="https://www.walgreens.com/findcare/vaccination/covid-19/location-screening"))
+    if post_response.ok != True:
+        print(post_response.status_code)
+        if post_response.status_code == 403:
+            client = bootstrap_walgreens(requests.Session())
+        print("Failed to connect to walgreens....")
+        time.sleep(90)
+        return
     dict_response = post_response.json()
     if dict_response['appointmentsAvailable'] == True:
-        sms("Walgreens has slots...!!")
-        #notify("Walgreens", "Has slots")
+        sms("Walgreens has slots...!! https://www.walgreens.com/findcare/vaccination/covid-19/location-screening")
+        
+    else:
+        print("...no slots at walgreens right now...")
+
+def bootstrap_walgreens(client):
+    #be polite and get this token...
+    resp = client.get("https://www.walgreens.com/browse/v1/csrf")
+    csrf = resp.json()
+    client.headers.update({csrf['csrfHeaderName']: csrf['csrfToken']})
+    return client
+
 
 def get_nav_urls(elems):
     ahrefs = elems.find_all('a')
     hrefs = [ ahref['href'] for ahref in ahrefs ]
     return hrefs
 
+def cvs(state):
+    headers = {"referer": "https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-link2-coronavirus-vaccine"}
+    url = f"https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{state}.json?vaccineinfo"
+    response = requests.get(url, headers = headers)
+
+    if (response.ok != True):
+        print("Failed to connect to CVS....")
+        time.sleep(90)
+        return
+    dict_response = response.json()
+
+    appointments = dict_response['responsePayloadData']['data'][state]
+    if (appointments):
+        available_appointments = [appointment for appointment in appointments if appointment['status'] != "Fully Booked"]
+        if (len(available_appointments) > 0):
+            print(available_appointments)
+            sms("CVS has" + str(available_appointments) + "slots! https://www.cvs.com/immunizations/covid-19-vaccine")
+        else:
+            print("No CVS appointments right now")
+
 
 if __name__ == "__main__":
     while True:
-        print("Gillette ->> https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Gillette&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
-        get_pages("Gillette","https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Gillette&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
-      #  #https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Fenway&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results
-      #  time.sleep(30)
-        print("Fenway ->> https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Fenway&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
-        get_pages("Fenway", "https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Fenway&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
-        walgreens()
-        #get_pages("ALL", "https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
+        #get_pages("ALL", "https://www.maimmunizations.org/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Fenway&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search#search_results")
+        client = requests.Session()
+        client = bootstrap_walgreens(client)
+        walgreens(client)
+        cvs("MA")
         time.sleep(60)
